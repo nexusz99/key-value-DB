1497d44501de6145f6e0f8a380f7a10b7743eb2e
nexusz99 <nexusz99@gmail.com>
2010-08-14 21:20:01 +0900

    네트워크 부분 구현

--- Client/src/client/Main.java
+++ Client/src/client/Main.java
@@ -1,21 +1,48 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package client;
 
-/**
- *
- * @author nexusz99
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        String ip="";
+        System.out.print("연결할 서버 IP : ");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        try
+        {
+            ip = in.readLine();
+            Init_Connection(ip);
+        }
+        catch(IOException e)
+        {
+        }
+        
+    }
+
+    static void Init_Connection(String ip)
+    {
+        Connect_Server server;
+        try
+        {
+            Socket sock = new Socket(ip,30621);
+            server = new Connect_Server(sock);
+            server.run();
+        }
+        catch(UnknownHostException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        catch(IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
     }
 
 }
