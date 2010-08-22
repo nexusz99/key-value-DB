729c7433c286e4fca9205bb0c0bb43606ed2abc6
nexusz99 <nexusz99@gmail.com>
2010-08-23 03:16:26 +0900

    Data 구조 업그레이드

--- Server/src/server/Data.java
+++ Server/src/server/Data.java
@@ -2,7 +2,9 @@ package server;
 
 public class Data{
     String cmd;
-    Object key;
-    Object value;
+    String key;
+    String value;
     int point;
+    boolean del;
+    boolean update;
 }
