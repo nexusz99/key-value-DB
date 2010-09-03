5ba22b61ba30f67d946eb80349d79bb0400baf58
nexusz99 <nexusz99@gmail.com>
2010-09-04 04:05:02 +0900

    Client 다시 제작

--- Client/src/client/Main.java
+++ Client/src/client/Main.java
@@ -1,73 +1,15 @@
 
 package client;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
-import java.net.UnknownHostException;
-
 public class Main {
 
-    public static Query_manager qm ;
-    public static Test_Socket test ;
-    public static Data data;
+    
     public static void main(String[] args) {
         
-        String input="";
-        //System.out.print("연결할 서버 IP : ");
-        qm = new Query_manager();
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        try
-        {
-            //input = in.readLine();
-            Init_Connection(input);
-            //test_init();
-        }
-        catch(Exception e)
-        {
-            System.exit(-1);
-        }
-
-        while(true)
-        {
-            System.out.print(">");
-            try
-            {
-                input = in.readLine(); 
-                test.Send(input);
-            }
-            catch(IOException e)
-            {
-                System.out.println(e.getStackTrace());
-            }
-        }
+        Test_Socket s = new Test_Socket();
+        s.initConnection();
+        s.startClient();
         
-    }
-
-    static void Init_Connection(String ip)
-    {
-        
-        try
-        {
-            Socket sock = new Socket("127.0.0.1",30621);
-            test = new Test_Socket(sock);
-            System.out.println("System Message : 소켓 생성 성공");
-        }
-        catch(UnknownHostException e)
-        {
-            System.out.println(e.getStackTrace());
-        }
-        catch(IOException e)
-        {
-            System.out.println(e.getStackTrace());
-        }
-
-    }
-
-    static void test_init()
-    {
-        qm = new Query_manager();
     }
 
 }
--- Client/src/client/Test_Socket.java
+++ Client/src/client/Test_Socket.java
@@ -1,24 +1,147 @@
 
 package client;
 
-import java.io.DataOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.Socket;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.util.Iterator;
 
 public class Test_Socket {
 
-    Socket sock;
-    DataOutputStream out;
-    public Test_Socket(Socket sock) throws IOException
-    {
-        this.sock = sock;
-        out = new DataOutputStream(this.sock.getOutputStream());
+    private static final String host = "localhost";
+    private static final int port = 30621;
+    
+    private Selector selector = null;
+    private SocketChannel sc = null;
+
+    private Charset charset = null;
+    private CharsetDecoder decoder = null;
+
+    public Test_Socket(){
+        charset = Charset.forName("EUC-KR");
+        decoder = charset.newDecoder();
     }
 
-    public void Send(String a) throws IOException
+    public void initConnection()
     {
-        out.flush();
-        out.writeUTF(a);
-        //out.flush();
+        try
+        {
+            selector = Selector.open();
+            sc = SocketChannel.open(new InetSocketAddress(host,port));
+            sc.configureBlocking(false);
+            sc.register(selector,SelectionKey.OP_READ);
+            System.out.println("서버 연결 완료");
+        }
+        catch(IOException e)
+        {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
     }
+    
+    public void startClient()
+    {
+        startWriter();
+        startReader();
+    }
+    
+    private void startWriter()
+    {
+        Thread t = new Writer(sc);
+        t.start();
+    }
+    private void startReader()
+    {
+        while(true)
+        {
+            try{
+                selector.select();
+                Iterator it = selector.selectedKeys().iterator();
+                while(it.hasNext())
+                {
+                    SelectionKey key = (SelectionKey)it.next();
+                    if(key.isReadable())
+                    {
+                        read(key);
+                    }
+                    it.remove();
+                }
+            }
+            catch(Exception e)
+            {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    
+    private void read(SelectionKey key)
+    {
+        SocketChannel sc2 = (SocketChannel)key.channel();
+        ByteBuffer buf = ByteBuffer.allocateDirect(1020);
+        try
+        {
+            sc2.read(buf);
+            buf.flip();
+            try
+            {
+                String data = decoder.decode(buf).toString();
+                System.out.println(data);
+            }
+            catch(CharacterCodingException a)
+            {
+                System.out.println(a.getMessage());
+            }
+        }
+        catch(IOException e)
+        {
+            try{
+                sc2.close();
+            }
+            catch(Exception a)
+            {}
+        }
+    }
+    
+}
+
+class Writer extends Thread
+{
+    private SocketChannel sc = null;
+    public Writer(SocketChannel s)
+    {
+        sc = s;
+    }
+    @Override
+    public void run() {
+       ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
+       BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+       while(!Thread.currentThread().isInterrupted())
+       {
+           try{
+               buffer.clear();
+               System.out.print(">");
+               String input = in.readLine();
+               buffer.put(input.getBytes());
+               buffer.flip();
+               sc.write(buffer);
+           }
+           catch(Exception e)
+           {
+               System.out.println(e.getMessage());
+           }
+           finally
+           {
+                buffer.clear();
+           }
+       }
+    }
+    
 }
